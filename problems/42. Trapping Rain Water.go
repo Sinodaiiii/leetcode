e9cc8ac11641386ae74a23db249918ac43c716c1
Sinodaiiii <ds_daisheng@163.com>
2025-05-25 23:05:15 +0800

    260525

---
+++ problems/42. Trapping Rain Water.go
@@ -0,0 +1,20 @@
+package problems
+
+func trap(height []int) int {
+	n := len(height)
+	l := make([]int, n)
+	l[0] = height[0]
+	for i := 1; i < n; i++ {
+		l[i] = max(l[i-1], height[i])
+	}
+	ans := 0
+	r := height[n-1]
+	for i := n - 2; i >= 1; i-- {
+		cur := min(l[i-1], r) - height[i]
+		if cur > 0 {
+			ans += cur
+		}
+		r = max(r, height[i])
+	}
+	return ans
+}
