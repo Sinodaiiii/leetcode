018de61ae4847245ded69fe2d09cea94839fb769
Sinodaiiii <ds_daisheng@163.com>
2025-07-11 22:19:56 +0800

    250711

--- hot100/448. Find All Numbers Disappeared in an Array.go
+++ hot100/448. Find All Numbers Disappeared in an Array.go
@@ -1,1 +1,20 @@
 package hot100
+
+func findDisappearedNumbers(nums []int) []int {
+    n := len(nums)
+    for i:=0; i<n; i++ {
+        if nums[i] != 0 {
+            j := nums[i]
+            for j!=0 {
+                j, nums[j-1] = nums[j-1], 0
+            }
+        }
+    }
+    ans := []int{}
+    for i:=0; i<n; i++ {
+        if nums[i] != 0 {
+            ans = append(ans, i+1)
+        }
+    }
+    return ans
+}
