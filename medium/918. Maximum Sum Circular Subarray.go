e27f626779aa0d1a24a7fb3ea92c8595967cfc06
Sinodaiiii <ds_daisheng@163.com>
2025-08-11 23:39:54 +0800

    250811

--- medium/918. Maximum Sum Circular Subarray.go
+++ medium/918. Maximum Sum Circular Subarray.go
@@ -1,17 +1,29 @@
 package medium
 
 func maxSubarraySumCircular(nums []int) int {
-	dp := nums[0]
-	ans := dp
 	n := len(nums)
-	for i := 1; i < n*2; i++ {
-		j := i % n
-		curr := dp + nums[j]
-		if i >= n {
-			curr -= nums[]
-		}
-		dp = max(dp+nums[j], nums[j])
-		ans = max(ans, dp)
+	ans := nums[0]
+	pre := nums[0]
+	for i := 1; i < n; i++ {
+		pre = max(pre+nums[i], nums[i])
+		ans = max(pre, ans)
+	}
+
+	l, r := make([]int, n), make([]int, n)
+	l[0] = nums[0]
+	r[n-1] = nums[n-1]
+	pre = nums[0]
+	for i := 1; i < n; i++ {
+		pre += nums[i]
+		l[i] = max(pre, l[i-1])
+	}
+	pre = nums[n-1]
+	for i := n - 2; i >= 0; i-- {
+		pre += nums[i]
+		r[i] = max(pre, r[i+1])
+	}
+	for i := 0; i < n-1; i++ {
+		ans = max(ans, l[i]+r[i+1])
 	}
 	return ans
 }
