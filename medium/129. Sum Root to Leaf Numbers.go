78ba7a38bdb2ff0a8e0eb8e0734a9019c69d3781
Sinodaiiii <ds_daisheng@163.com>
2025-11-09 22:26:38 +0800

    251109

---
+++ medium/129. Sum Root to Leaf Numbers.go
@@ -0,0 +1,32 @@
+package medium
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumNumbers(root *TreeNode) int {
+	ans := 0
+	curr := 0
+	var dfs func(root *TreeNode)
+	dfs = func(root *TreeNode) {
+		curr = curr*10 + root.Val
+		if root.Left == nil && root.Right == nil {
+			ans += curr
+		} else {
+			if root.Left != nil {
+				dfs(root.Left)
+			}
+			if root.Right != nil {
+				dfs(root.Right)
+			}
+		}
+		curr = curr / 10
+	}
+
+	dfs(root)
+	return ans
+}
