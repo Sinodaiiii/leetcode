bdd5567af49d163d3c24dabee2723e606e562710
Sinodaiiii <ds_daisheng@163.com>
2026-05-05 16:39:39 +0800

    260505

---
+++ easy/3740. Minimum Distance Between Three Equal Elements I.go
@@ -0,0 +1,20 @@
+package easy
+
+import "math"
+
+func minimumDistance260505(nums []int) int {
+	ans := math.MaxInt32
+	dict := make([][2]int, len(nums)+1)
+	for i, num := range nums {
+		if dict[num][1] != 0 {
+			ans = min(ans, 2*(i-dict[num][0]+dict[num][1]))
+			dict[num] = [2]int{i, i - dict[num][0]}
+		} else {
+			dict[num] = [2]int{i, math.MaxInt32}
+		}
+	}
+	if ans == math.MaxInt32 {
+		return -1
+	}
+	return ans
+}
